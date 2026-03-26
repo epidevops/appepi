46cdd35f4b349d0c831c3922155f3ae3de9d9ef4
epidevops <epidevops@gmail.com>
2026-03-26 03:52:08 -0600

    Feat: Auth Job Sqlite

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :accounts
-  get "home/index"
+  resource :session
+  resources :passwords, param: :token
+
+  resource :dashboard, only: :show
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -13,4 +15,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index"
+
+  mount SolidLitequeen::Engine, at: "/sqlite", as: :mount_sqlite
+  mount MissionControl::Jobs::Engine, at: "/jobs", as: :mount_jobs
 end
