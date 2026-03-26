db2579d1c78ceed617f378328c612ee992fd3f09
epidevops <epidevops@gmail.com>
2026-03-26 16:37:07 -0600

    Feat: Add git leaks

--- config/ci.rb
+++ config/ci.rb
@@ -5,9 +5,11 @@ CI.run do
 
   step "Style: Ruby", "bin/rubocop"
 
-  step "Security: Gem audit", "bin/bundler-audit"
-  step "Security: Importmap vulnerability audit", "bin/importmap audit"
-  step "Security: Brakeman code analysis", "bin/brakeman --quiet --no-pager --exit-on-warn --exit-on-error"
+  step "Security: Gem audit", "bin/bundler-audit check --update"
+  step "Security: Importmap audit", "bin/importmap audit"
+  step "Security: Brakeman audit", "bin/brakeman --quiet --no-pager --exit-on-warn --exit-on-error"
+  step "Security: Gitleaks audit", "bin/gitleaks-audit"
+
   step "Tests: Rails", "bin/rails test"
   step "Tests: Seeds", "env RAILS_ENV=test bin/rails db:seed:replant"
 
